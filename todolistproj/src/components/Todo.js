9bf3f355b8c679740aa506d9a5af4775636bf580
fleuraje <127205495+fleuraje@users.noreply.github.com>
2023-12-28 05:24:01 -0500

    update task function added

--- todolistproj/src/components/Todo.js
+++ todolistproj/src/components/Todo.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { EditTodoForm } from './EditTodoForm';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export const Todo = ({ task, deleteTodo, editTodo, toggleComplete }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+  };
+
+  const handleEditTodo = (editedValue) => {
+    editTodo(editedValue, task.id);
+    setIsEditing(false);
+  };
+
   return (
     <div className="Todo">
-      <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>
-        {task.task}
-      </p>
-      <div>
-        <FontAwesomeIcon className="edit-icon" icon={faPenSquare} onClick={() => editTodo(task.id)} onClickCapture={() => editTodoForm(task.id)} />
-        <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
-      </div>
+      {isEditing ? (
+        <EditTodoForm task={task} editTodo={handleEditTodo} onCancel={handleCancelEdit} />
+      ) : (
+        <>
+          <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>
+            {task.task}
+          </p>
+          <div>
+            <FontAwesomeIcon className="edit-icon" icon={faPenSquare} onClick={handleEditClick} />
+            <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
